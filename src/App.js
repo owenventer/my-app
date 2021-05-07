a7bda5cc25a91ffe2ddcf9d1010614ef6680addf
Owen Venter <owen@Owens-MacBook-Pro-2.local>
2021-05-07 13:37:46 +0200

    owen tests deez nuts

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p style={{"color": "royalblue"}}><h1>David's toast is always very poes cold and hard</h1></p>
+        <p style={{"color": "royalblue"}}><h1>I really fucking need tea</h1></p>
         <img src={logo} className="App-logo" alt="logo" />
         
         <p>
           
-          <b><font color="blue"> ICE COLD AND ROCK HARD</font></b>
+          <b><font color="blue"> Nice hot tea</font></b>
         </p>
         <a
           className="App-link"
