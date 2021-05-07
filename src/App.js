eaed84b969cc21d081739d9f0c68b761d6936176
Owen Venter <owen@Owens-MacBook-Pro-2.local>
2021-05-07 12:19:28 +0200

    fresh changes

--- src/App.js
+++ src/App.js
@@ -5,16 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-<<<<<<< HEAD
-        <font color="red"><h1>David's toast is frozen</h1></font>
-=======
-        <font color="red"><h1>David's toast is now cold and hard</h1></font>
->>>>>>> 9f11449b916eeb98c09df993507a352c94731255
+
+        <font color="red"><h1>Owen's tea is finnished</h1></font>
+
         <img src={logo} className="App-logo" alt="logo" />
         
         <p>
           
-          <b><font color="blue"> ICE COLD AND ROCK HARD</font></b>
+          <b><font color="blue"> Empty BOI</font></b>
         </p>
         <a
           className="App-link"
