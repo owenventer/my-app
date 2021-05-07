fee74058e2918df6a6423b7e45ee183402fef5f8
Nivan Poken <nivanpkn@gmail.com>
2021-05-07 14:07:46 +0200

    link open in new tab

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
         >
           Press F to pay respects
         </a>
-        <a href="https://google.com">Click me</a>
+        <a href="https://google.com" target="blank">Click me</a>
       </header>
     </div>
   );
