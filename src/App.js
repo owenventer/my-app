2e65563999d9533b94acc3966efedb223617af96
David Pullinger <david@Davids-MacBook-Pro.local>
2021-05-07 13:45:08 +0200

    David test 2

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p style={{"color": "royalblue"}}><h1>I really fucking need tea</h1></p>
+        <h1 style={{"color": "royalblue"}}>David's toast is always very poes cold and hard</h1>
+        <p>Some test text</p>
         <img src={logo} className="App-logo" alt="logo" />
-        
         <p>
-          
-          <b><font color="blue"> Nice hot tea</font></b>
+          <b><font color="blue"> ICE COLD AND ROCK HARD</font></b>
         </p>
         <a
           className="App-link"
